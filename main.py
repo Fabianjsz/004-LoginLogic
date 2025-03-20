3392bea09510b2ee2e797817571b1d648f844608
Fabibi <fa2006bian@gmail.com>
2025-03-20 21:01:44 +0100

    added hashing to the passwords and a hashing function

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+import hashlib
 
-users = ["Fabian", "Emmi"]
-passwords = ["Emmi0707", "penispenis123"]
+
+
+users = ["Emmi", "Fabian"]
+passwords = ["8fe29b9687d13858578b19e2de262c42d1ad13c9e50aea5c8a4f50aece775479", "112851505944c52438aee6c8b39e3cc4cfed418cff97fb73b862b63d8b459aec"]
+
+
+def passwordHasher(pw):
+    hashed = hashlib.sha256(pw.encode("utf-8"))
+    hashed = hashed.hexdigest()
+    return(hashed)
+
+
+
 
 def is_valid_credentials(u, pw):
     if u in users:
         index = users.index(u)
-        if pw == passwords[index]:
-            if u == "Fabian":
-                print("I love my girlfriend more than anything <3")
-            elif u == "Emmi":
-                print("I love my Boyfriend >:(")
+        if str(passwordHasher(pw)) == passwords[index]:
+            return True
+    else:   
+        return False
     
-    else:
-        print("Wrong credentials, please try again")
-    
+
+
+
+
+
 user = input("what is your name? ")
 password = input("Password: ")
 
-is_valid_credentials(user, password)
+print(is_valid_credentials(user, password))
+
+
